63677071975b49f7778e796fd4f4680b3ae0a52e
Abdalla19981 <322869@student.mboutrecht.nl>
2021-12-15 16:36:03 +0100

    initial commit

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php 
+
+$severname = "localhost";
+$username = "root";
+$password = "";
+$databasename ="nike";
+ 
+
+$coon = mysqli_connect( $severname, $username , $password , $databasename );
+
+
+$sql =  "INSERT INTO `users` (`id`, `firstname`, `email`, `nummer`, `sms` )
+ VALUES (NULL, '{$_POST ["firstname"]}', '{$_POST["email"]}', '{$_POST["nummer"]}', '{$_POST["sms"]}'  );";
+
+mysqli_query($coon , $sql );
+
+
+  header("Refresh:3; url=./index.html");
+
+?>
+<link rel="stylesheet" href="style.css">
+
+<div class="center">
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
+
