ea3b63cc005f202439c71ef1d673b84b5751bd5f
Abdalla19981 <322869@student.mboutrecht.nl>
2022-01-11 15:34:15 +0100

    dit is edit review

---
+++ review.php
@@ -0,0 +1,34 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename ="nike";
+ 
+
+$conn = mysqli_connect( $servername, $username , $password , $databasename );
+
+
+$sql =  "INSERT INTO `review` (`id`, `firstname`, `sms`, `nummer`, `email` )
+ VALUES (NULL, '{$_POST["firstname"]}', '{$_POST["sms"]}', '{$_POST["nummer"]}', '{$_POST["email"]}'  );";
+
+mysqli_query($conn , $sql );
+
+
+ header("Refresh:3; url=./index.html");
+
+?>
+<link rel="stylesheet" href="style.css">
+
+<div class="center">
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
